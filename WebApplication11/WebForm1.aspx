be94ec0a1034b0613664b00847d48f52d8ddd172
riddhijani <46336235+riddhijani@users.noreply.github.com>
2019-04-04 09:44:22 +0530

    Add files via upload

---
+++ WebApplication11/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication11.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="texta" runat="server" ontextchanged="TextBox1_TextChanged"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="textb" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" onclick="btnSub_Click" Text="+" />
+        <asp:Button ID="Button2" runat="server" onclick="btnAdd_Click" Text="-" />
+        <asp:Button ID="Button3" runat="server" onclick="btnMult_Click" Text="*" />
+        <asp:Button ID="Button4" runat="server" onclick="btnDivide_Click" Text="/" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label1"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication11/WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication11
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        webservice.WebService1 cal = new webservice.WebService1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSub_Click(object sender, EventArgs e)
+        {
+           Label1.Text = cal.Add(Convert.ToInt16(texta.Text), Convert.ToInt16(textb.Text)).ToString();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+           Label1.Text = cal.Subtract(Convert.ToInt16(texta.Text), Convert.ToInt16(textb.Text)).ToString();
+            
+        }
+
+        protected void btnMult_Click(object sender, EventArgs e)
+        {
+
+            Label1.Text = cal.Multiply(Convert.ToInt16(texta.Text), Convert.ToInt16(textb.Text)).ToString();
+        }
+
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+          
+            Label1.Text = cal.Divide(Convert.ToInt16(texta.Text), Convert.ToInt16(textb.Text)).ToString();
+        }
+    }
+}
